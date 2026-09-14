a1c3785a1864414f23e265b5bde465fcb5cfddba
seifdyago <seifdyago@gmail.com>
2026-09-15 02:04:02 +0300

    Update build.mjs

--- artifacts/api-server/build.mjs
+++ artifacts/api-server/build.mjs
@@ -5,28 +5,40 @@ import { build as esbuild } from "esbuild";
 import esbuildPluginPino from "esbuild-plugin-pino";
 import { rm } from "node:fs/promises";
 
-// Plugins (e.g. 'esbuild-plugin-pino') may use `require` to resolve dependencies
 globalThis.require = createRequire(import.meta.url);
 
 const artifactDir = path.dirname(fileURLToPath(import.meta.url));
 
 async function buildAll() {
   const distDir = path.resolve(artifactDir, "dist");
-  await rm(distDir, { recursive: true, force: true });
+
+  await rm(distDir, {
+    recursive: true,
+    force: true,
+  });
 
   await esbuild({
-    entryPoints: [path.resolve(artifactDir, "src/index.ts")],
+    entryPoints: [
+      path.resolve(
+        artifactDir,
+        "src/app.ts",
+      ),
+    ],
+
     platform: "node",
+
     bundle: true,
+
     format: "esm",
+
     outdir: distDir,
-    outExtension: { ".js": ".mjs" },
+
+    outExtension: {
+      ".js": ".mjs",
+    },
+
     logLevel: "info",
-    // Some packages may not be bundleable, so we externalize them, we can add more here as needed.
-    // Some of the packages below may not be imported or installed, but we're adding them in case they are in the future.
-    // Examples of unbundleable packages:
-    // - uses native modules and loads them dynamically (e.g. sharp)
-    // - use path traversal to read files (e.g. @google-cloud/secret-manager loads sibling .proto files)
+
     external: [
       "*.node",
       "sharp",
@@ -101,12 +113,15 @@ async function buildAll() {
       "puppeteer-core",
       "electron",
     ],
+
     sourcemap: "linked",
+
     plugins: [
-      // pino relies on workers to handle logging, instead of externalizing it we use a plugin to handle it
-      esbuildPluginPino({ transports: ["pino-pretty"] })
+      esbuildPluginPino({
+        transports: ["pino-pretty"],
+      }),
     ],
-    // Make sure packages that are cjs only (e.g. express) but are bundled continue to work in our esm output file
+
     banner: {
       js: `import { createRequire as __bannerCrReq } from 'node:module';
 import __bannerPath from 'node:path';
@@ -115,7 +130,7 @@ import __bannerUrl from 'node:url';
 globalThis.require = __bannerCrReq(import.meta.url);
 globalThis.__filename = __bannerUrl.fileURLToPath(import.meta.url);
 globalThis.__dirname = __bannerPath.dirname(globalThis.__filename);
-    `,
+`,
     },
   });
 }
